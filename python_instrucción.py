33ed89ef29322cd3aeb1e7be3939e777021cb385
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-06 02:46:13 +0100

    Create python_instrucción.py

---
+++ python_instrucción.py
@@ -0,0 +1,11 @@
+print(""" Hola Mundo!\n
+
+Aqui pondre todos mis cursos sobre Python.
+de principiante hasta avanzado\n.
+
+Hare lo mismo con todos los lenguajes
+Python al ser el mas facil sera el mas mantenido
+(por logica)
+
+
+""")
