217bcd3201eed44ecad77e057df7abe2635b8229
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-09 06:49:36 +0100

    Add examples of list operations in Python
    Demonstrates various list operations in Python, including creation, addition, insertion, removal, and slicing.

---
+++ Python/Listas/sintexis_listas.py
@@ -0,0 +1,41 @@
+# dos formas de crear listas
+lista_nombre = []
+lista = list()
+
+# tipo
+print(type(lista_nombre))
+print(type(lista))
+
+# agregar elemento
+lista_nombre.append("Jonathan")
+lista_nombre.append("Angeline")
+print(lista_nombre)
+
+# insertar posición exacta
+lista_nombre.insert(1,"y")
+print(lista_nombre)
+
+# eliminar último elemento
+lista_nombre.pop()
+print(lista_nombre)
+
+# eliminar
+lista_nombre.remove("y")
+print(lista_nombre)
+
+# agrega una lista a la lista 
+lista_nombre.extend(["Angeline","Francia", "Ecuador", "Paris", "Madrid"])
+print(lista_nombre)
+
+# muestra el index 
+print(lista_nombre.index("Francia"))
+
+# imprime el valor 
+print(lista_nombre[2])
+
+# imprime el valor desde el indice 2 al 5 el 6 es excluyente
+print(lista_nombre[2:6])
+
+# borra todo
+lista_nombre.clear()
+print(lista_nombre)
