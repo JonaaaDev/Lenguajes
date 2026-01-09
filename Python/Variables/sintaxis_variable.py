d13fa36378315d342ed9e70eee71ffb174faac6e
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-09 06:51:47 +0100

    Update and rename 01_uso_variable.py to sintaxis_variable.py

--- Python/Variables/sintaxis_variable.py
+++ Python/Variables/sintaxis_variable.py
@@ -7,5 +7,5 @@ edad = 19 #tipo integer «int» significa numero entero
 
 salario = 1200.0 #tipo float «float» significa numero con decimal
 
-trabajando = True #tipo boolean «bool» significa verdad
-estudiando = False #tipo boolean «bool» significa falso
+trabajando = False #tipo boolean «bool» significa verdad
+estudiando = True #tipo boolean «bool» significa falso
