5fdcebb9ff37c23bd57b6e14b770a1d732160df2
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-12 16:03:36 +0100

    Create uso_for-else.py

---
+++ Python/For/uso_for-else.py
@@ -0,0 +1,22 @@
+# caso 1
+print("Caso 1")
+
+# bucle range del 0 al 4
+for i in range(5):
+    if i == 10:
+        print(10)
+else:
+    print("No se encontro\n")        
+
+#____________________________________________
+
+# caso 2    
+print("Caso 2")
+
+# bucle iterando la palabra «Angeline»
+for i in "Angeline":
+    if i == "i":
+        print("Se encontró la I")
+        break
+else:
+    print("No se encontro")            
