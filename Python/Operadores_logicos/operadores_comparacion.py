46716d4b82777d39dde1c68582f489313fa877dd
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-14 00:00:38 +0100

    Create  operadores_comparacion.py

---
+++ Python/Operadores_logicos/operadores_comparacion.py
@@ -0,0 +1,40 @@
+# ¡operadores lógicos!
+
+# operador «>» -> mayor que.
+
+# operador «<» -> menor que.
+
+# operador «>=» -> mayor o igual que.
+
+# operador «<=» -> menor o igual que.
+
+# operador «==» -> igual que.
+
+# operador «!=» -> diferente que.
+
+
+# ejemplos básico:
+
+# miramos si es mayor
+print(4 > 3) # ¿4 mayor que 3? True
+
+# miramos si es menor
+print(4 < 3) # ¿4 menor que 3? False
+
+# miramos si es mayor o igual
+print(10 >= 10) # ¿10 mayor o igual que 10? True
+
+# miramos si es menor o igual
+print(8 <= 10) # ¿8 menor o igual que 10? True
+
+# miramos si es menor o igual
+print(28 <= 20) # ¿28 menor o igual que 20? False
+
+# miramos si es igual 
+print(2.3 == 2) # ¿2.3 es igual que 2? False
+
+# miramos si es igual
+print(444 == 444) # ¿444 es igual que 444? True
+
+# miramos si es distinto
+print(20 != 19) # ¿20 es distinto que 19? True
