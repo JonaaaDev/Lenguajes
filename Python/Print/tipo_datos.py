a031bdba43f3917c28b4a1c221b02153ad214e08
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-13 23:37:04 +0100

    Create tipo_datos.py

---
+++ Python/Print/tipo_datos.py
@@ -0,0 +1,59 @@
+# clase 1
+# nombre entero -> String
+# tipo -> str
+
+# para imprimir texto (str/strings) se pone entre comillas
+# pueden ser comillas simples o dobles
+
+# para imprimir un solo caracter prefiero comillas simples
+print('a') 
+print('1') # si está entre comillas siempre será considerado (strings) texto
+
+# para imprimir una lista de caracteres (texto)
+# prefiero comillas dobles
+print("Hola Mundo")
+print("123") #!si está entre comillas siempre será considerado (strings) texto
+
+# lo de las comillas dobles o simples, no es obligatorio 
+# no hay una forma correcta, simplemente estoy acostumbrado así
+
+
+# clase 2
+# nombre entero -> Integer
+# tipo -> int 
+
+# imprimir numeros (int/integer) ¡No se debe de poner entre comillas!
+
+print(1) # si se pone entre comillas será considerado como string y no integer
+print(9999) # no hay límite de numero
+print(-70) # puede ser número negativo
+# no hay limitaciones, puede ser negativo o positivo
+
+
+# clase 3
+# nombre entero -> Floating point
+# tipo -> float
+
+# imprimir números con decimales (float) no se debe de poner entre comillas tampoco 
+# es lo mismo que los int pero con decimal
+# para indicar la decimal se pone un punto, ¡no una coma!
+
+print(10.1) # si se pone una coma para indicar la decima
+# el programa entenderá que quieres dos números por separado de tipo int
+
+# puede ser negativo o positivo, igual que los int
+print(-55.5) 
+
+
+# clase 4
+# nombre entero -> Boolean
+# tipo -> bool
+
+# ¿Para que sirven los boolean?
+# solo pueden contener True o False (verdadero o falso)
+# se escriben con la primera letra en mayúsculas «True/False» 
+print(True)
+print(False)
+
+# a estas alturas es dificil entender para que sirven
+# pero está bien tenerlo en cuenta ahora, para más tarde
