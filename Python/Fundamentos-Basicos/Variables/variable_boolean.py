de075590e3995ce238ef9714ad3deca4cd7074b3
JonaaaDev <jonathangimenezdev@gmail.com>
2026-01-14 02:07:05 +0100

    Create variable_boolean.py

---
+++ Python/Fundamentos-Basicos/Variables/variable_boolean.py
@@ -0,0 +1,20 @@
+# guardar booleans (verdadero o falso)
+
+# las variables de tipo «bool» solo pueden tener dos estados: True o False
+# son como interruptores: activo/desactivo, sí/no, abierto/cerrado
+
+# ejemplo de estado de un videojuego
+esta_vivo = True
+partida_pausada = False
+
+# ejemplo de permisos de usuario
+tiene_premium = True
+es_administrador = False
+
+# ¡Recordad!: la primera letra DEBE ser mayúscula (True / False)
+# Si lo escribes en minúscula (true), Python te dará un error
+# Si lo pones entre comillas ("True"), dejará de ser un bool y será un «str»
+
+# estas variables son muy útiles para cambiar el flujo del programa
+# por ejemplo, si el jugador toca un pincho:
+esta_vivo = False
